15255d79663054b7cd5550a31d1e116c9d36efb0
Libor Matějek <lmatejek1@gmail.com>
2021-10-07 09:00:14 +0200

    cv03

--- cv03/Src/main.c
+++ cv03/Src/main.c
@@ -19,26 +19,36 @@
 
 #include <stdint.h>
 #include "stm32f0xx.h"
+#include "sct.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
+
+
 int main(void)
 {
+	sct_init();
+	/*sct_led(0x7A5C36DE);*/
 
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODER5_0;
-	uint32_t sos = 0b10101001110111011100101010000000;
 
-    /* Loop forever */
-	for(;;)
-	{
-		for(uint32_t i = 0;i<32;i++)
-		{
-			if(sos & (1U<<(31-i))){GPIOA->BSRR = (1<<5);}
-			else{GPIOA->BRR = (1<<5);}
-			for (volatile uint32_t i=0; i<10000;i++){}
+
+	while(1){
+
+
+		for(uint32_t i = 0;i<999;i=i+111){
+			sct_value(i);
+			for(uint32_t j = 0;j<100000;j++){
+			}
 		}
+
+
 	}
+
+
+
 }
+
+
