baabb7b9c49f16d70efc9f59dc8391c72f8acf20
Libor Matějek <lmatejek1@gmail.com>
2021-10-07 07:28:38 +0200

    cv03

---
+++ cv03/Src/sct.c
@@ -0,0 +1,13 @@
+/*
+ * sct.c
+ *
+ *  Created on: 7. 10. 2021
+ *      Author: student
+ */
+
+#ifndef SCT_C_
+#define SCT_C_
+
+
+
+#endif /* SCT_C_ */
---
+++ cv03/Src/sct.h
@@ -0,0 +1,13 @@
+/*
+ * sct.h
+ *
+ *  Created on: 7. 10. 2021
+ *      Author: student
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+
+#endif /* SCT_H_ */
